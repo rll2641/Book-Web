d70189d0af9bf932d6fcc6e40f02bc595688c283
JiHoon <fair0838@gmail.com>
2025-08-02 15:03:19 +0900

    update :

---
+++ src/main/java/com/fastcampus/book_bot/BookBotApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.book_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookBotApplication.class, args);
+    }
+
+}
